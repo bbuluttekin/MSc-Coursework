212d4102807a8e71939f92bad70043290285de4d
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2018-12-07 17:34:18 +0000

    Extending gitignore and more modular gradient function

--- PwD/gradient_numpy.py
+++ PwD/gradient_numpy.py
@@ -4,18 +4,18 @@ import numpy as np
 import sys
 
 
-def grad_desc(x, y):
+def grad_desc(x, y, l_rate=0.00001, limit_err=0.001):
     # Looking for y = m*x + b => (I know that it should be y = 2x+3)
     results = []
     converged = False
     m, b = 0, 0
     count = 0
     n = len(x)
-    learning_rate = 0.00001
+    learning_rate = l_rate
 
     e = sys.maxsize  # Maxsize intiger of python
     iter = 10000
-    limit_error = 0.001
+    limit_error = limit_err
     while not converged:
         count += 1
         y_predicted = m * x + b
@@ -35,9 +35,16 @@ def grad_desc(x, y):
     results.append([count, m, b, mse])
     print("f(x) =  {}* x + {}, with mse {} after iteration {}".format(round(m),
                                                                       round(b, 0), round(mse, 5), count))
+    return mse
 
 
 x = np.array([1, 2, 3, 4, 5])
 y = np.array([5, 7, 9, 11, 13])
 
-grad_desc(x, y)
+
+min_mse = {}
+for i in [0.01, 0.001, 0.0001, 0.000001]:
+    min_mse[grad_desc(x, y, limit_err=i)] = i
+
+print("Result:")
+print(min_mse[min(min_mse.keys())])
