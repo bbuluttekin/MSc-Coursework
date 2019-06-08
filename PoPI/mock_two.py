50e6bc69b343b46562f1b19af97a26541e438023
Baran Buluttekin <bbuluttekin@users.noreply.github.com>
2019-06-08 23:14:25 +0100

    Recursive power function

--- PoPI/mock_two.py
+++ PoPI/mock_two.py
@@ -2,5 +2,13 @@ def sqrProd(x, y):
     return (x * y) ** 2
 
 
+def power(a, n):
+    if n == 0:
+        return 1
+    else:
+        return power(a, n - 1) * a
+
+
 if __name__ == "__main__":
     assert sqrProd(2, 5) == 100
+    assert power(2, 3) == 8
